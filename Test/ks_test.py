833bc811ede91db76866e26b59434ee21049e563
Sishuo Wang <sishuowang@hotmail.ca>
2015-04-12 09:46:29 -0700

    Test

---
+++ Test/ks_test.py
@@ -0,0 +1,23 @@
+#! /bin/env python
+
+# script to do Kormogorov-Simirnov test
+
+#####################################################
+from scipy.stats import ks_2samp
+import sys
+
+#####################################################
+def read_list(list_file):
+    list = []
+    for line in open(list_file, 'r').readlines():
+        line = line.rstrip('\n\r')
+        list.append(float(line))
+    return(list)
+
+
+#####################################################
+list1 = read_list(sys.argv[1])
+list2 = read_list(sys.argv[2])
+
+print ks_2samp(list1, list2)[1]
+
