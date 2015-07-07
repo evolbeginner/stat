d75e44a449f1e00e48feebfaef486344375a55e2
Sishuo Wang <sishuowang@hotmail.ca>
2015-07-07 10:55:14 -0700

    Test

--- Test/calculate_correl.py
+++ Test/calculate_correl.py
@@ -8,6 +8,17 @@ import re
 
 
 ##########################################################################
+inputs = []
+fields = []
+field_sep = " "
+
+is_pearson = True
+is_spearman = True
+is_only_p_value = False
+is_with_title = False
+
+
+##########################################################################
 def read_input(inputs, fields, field_sep):
     values = {}
     for index_1, input in enumerate(inputs):
@@ -25,16 +36,25 @@ def read_input(inputs, fields, field_sep):
     return(values)
 
 
+def get_correl_coeffieicnt(method, values_0, values_1, is_only_p_value):
+    result = None
+    if method == "pearson":
+        result = pearsonr(values_0, values_1)
+    elif method == "spearman":
+        result = spearmanr(values_0, values_1)
+    else:
+        raise "Unknown method for calculating correlation coefficients. Exiting ......"
+
+    if is_only_p_value:
+        result = result[1]
+    return(result)
+        
+
 ##########################################################################
-inputs = []
-fields = []
-field_sep = " "
-
-
 opts, args = getopt.getopt(
     sys.argv[1:],
     'i:f:',
-    ['in=', 'field=', 'sep=', 'field_sep=']
+    ['in=', 'field=', 'sep=', 'field_sep=', "no_pearson", "no_spearman", "only_p_value", "only_p-value", "with_title"]
 )
 
 
@@ -43,25 +63,40 @@ for opt, value in opts:
         for i in value.split(','):
             inputs.append(i)
     elif opt == '-f' or opt == '--field':
-      for index, i in enumerate(value.split(';')):
-        fields.append([])
-        for j in i.split(','):
-          fields[index].append(int(j)-1)
+        for index, i in enumerate(value.split(';')):
+            fields.append([])
+            for j in i.split(','):
+                fields[index].append(int(j)-1)
     elif opt == '-f' or opt == '--field':
-      field_sep = value
+        field_sep = value
+    elif opt == "--no_pearson":
+        is_pearson = False
+    elif opt == "--no_spearman":
+        is_spearman = False
+    elif opt == "--only_p_value" or opt == "--only_p-value":
+        is_only_p_value = True
+    elif opt == "--with_title":
+        is_with_title = True
 
 
 if not fields:
-  if len(inputs) == 2:
-    fields = [[0],[0]]
-  elif len(inputs) == 1:
-    fields = [[0,1]]
+    if len(inputs) == 2:
+        fields = [[0],[0]]
+    elif len(inputs) == 1:
+        fields = [[0,1]]
 
 
 ##########################################################################
 values = read_input(inputs, fields, field_sep)
 
-print pearsonr(values.values()[0], values.values()[1])
-print spearmanr(values.values()[0], values.values()[1])
+if is_pearson:
+    if is_with_title:
+        print "pearson"+"\t",
+    print get_correl_coeffieicnt('pearson', values.values()[0], values.values()[1], is_only_p_value)
+if is_spearman:
+    if is_with_title:
+        print "spearman"+"\t",
+    print get_correl_coeffieicnt('spearman', values.values()[0], values.values()[1], is_only_p_value)
+    
 
 
